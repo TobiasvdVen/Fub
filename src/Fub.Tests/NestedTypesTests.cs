74a8a0fd5fdd0eb5f88deae29fa77260081ff6e7
Tobias van de Ven <tobiasvdven@hotmail.com>
2022-03-13 20:46:19 +0100

    Started NestedTypesTests

---
+++ src/Fub.Tests/NestedTypesTests.cs
@@ -0,0 +1,70 @@
+﻿using Xunit;
+
+namespace Fub.Tests
+{
+	public class NestedTypesTests
+	{
+		public class Empty
+		{
+
+		}
+
+		public class HasEmptyNestedType
+		{
+			public HasEmptyNestedType(Empty empty)
+			{
+				Empty = empty;
+			}
+
+			public Empty Empty { get; }
+		}
+
+		[Fact]
+		public void Create_HasEmptyNestedType_ReturnsDefault()
+		{
+			FubBuilder<HasEmptyNestedType> builder = new();
+			Fub<HasEmptyNestedType> fub = builder.Build();
+
+			HasEmptyNestedType created = fub.Create();
+
+			Assert.NotNull(created.Empty);
+		}
+
+		public class Simple
+		{
+			public Simple(string name, bool boolean, Empty empty)
+			{
+				Name = name;
+				Boolean = boolean;
+				Empty = empty;
+			}
+
+			public string Name { get; }
+			public bool Boolean { get; }
+			public Empty Empty { get; }
+		}
+
+		public class HasSimpleNestedType
+		{
+			public HasSimpleNestedType(Simple simple)
+			{
+				Simple = simple;
+			}
+
+			public Simple Simple { get; }
+		}
+
+		[Fact]
+		public void Create_HasSimpleNestedType_WithNoOverrides_ReturnsDefault()
+		{
+			FubBuilder<HasSimpleNestedType> builder = new();
+			Fub<HasSimpleNestedType> fub = builder.Build();
+
+			HasSimpleNestedType created = fub.Create();
+
+			Assert.Equal(string.Empty, created.Simple.Name);
+			Assert.Equal(default, created.Simple.Boolean);
+			Assert.NotNull(created.Simple.Empty);
+		}
+	}
+}
