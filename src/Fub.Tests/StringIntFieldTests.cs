3c123584f66830b8e2a264305f44b8fea0e19c42
Tobias van de Ven <tobiasvdven@hotmail.com>
2022-03-13 19:00:20 +0100

    StringIntFieldTests

---
+++ src/Fub.Tests/StringIntFieldTests.cs
@@ -0,0 +1,75 @@
+﻿using Xunit;
+
+namespace Fub.Tests
+{
+	public class StringIntFieldTests
+	{
+		public class Class
+		{
+			public string @string;
+			public int integer;
+
+			public Class(string @string, int integer)
+			{
+				this.@string = @string;
+				this.integer = integer;
+			}
+		}
+
+		public struct Struct
+		{
+			public string @string;
+			public int integer;
+		}
+
+		public struct StructWithConstructor
+		{
+			public string @string;
+			public int integer;
+
+			public StructWithConstructor(string @string, int integer)
+			{
+				this.@string = @string;
+				this.integer = integer;
+			}
+		}
+
+		[Fact]
+		public void Create_ClassWithNoOverrides_ReturnsDefault()
+		{
+			FubBuilder<Class> builder = new();
+			Fub<Class> fub = builder.Build();
+
+			Class created = fub.Create();
+
+			Assert.NotNull(created.@string);
+			Assert.Equal(string.Empty, created.@string);
+			Assert.Equal(default, created.integer);
+		}
+
+		[Fact]
+		public void Create_StructWithNoOverrides_ReturnsDefault()
+		{
+			FubBuilder<Struct> builder = new();
+			Fub<Struct> fub = builder.Build();
+
+			Struct created = fub.Create();
+
+			Assert.Null(created.@string);
+			Assert.Equal(default, created.integer);
+		}
+
+		[Fact]
+		public void Create_StructWithConstructorWithNoOverrides_ReturnsDefault()
+		{
+			FubBuilder<StructWithConstructor> builder = new();
+			Fub<StructWithConstructor> fub = builder.Build();
+
+			StructWithConstructor created = fub.Create();
+
+			Assert.NotNull(created.@string);
+			Assert.Equal(string.Empty, created.@string);
+			Assert.Equal(default, created.integer);
+		}
+	}
+}
