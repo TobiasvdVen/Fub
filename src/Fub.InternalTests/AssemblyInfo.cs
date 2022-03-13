d1bd69a2e3b789ddcc4b4ae036c618d52e73de2b
Tobias van de Ven <tobiasvdven@hotmail.com>
2022-03-13 13:38:59 +0100

    Added NCrunch engine mode to not auto-run all targets

---
+++ src/Fub.InternalTests/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using NCrunch.Framework;
+
+#if NET6_0
+[assembly: Category("net6.0")]
+#endif
+
+#if NET5_0
+[assembly: Category("net5.0")]
+#endif
+
+#if NET48
+[assembly: Category("net48")]
+#endif
+
+#if NET472
+[assembly: Category("net472")]
+#endif
+
+#if NET461
+[assembly: Category("net461")]
+#endif
