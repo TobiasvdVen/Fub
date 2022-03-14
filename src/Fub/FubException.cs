b326d2d4fe26a7e1b15a41ac4bb30594b3b4f960
Tobias van de Ven <tobiasvdven@hotmail.com>
2022-03-14 21:50:22 +0100

    FubException

---
+++ src/Fub/FubException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Fub
+{
+	public class FubException : Exception
+	{
+		public FubException(string? message) : base(message)
+		{
+		}
+
+		public FubException(string? message, Exception? innerException) : base(message, innerException)
+		{
+		}
+	}
+}
