518f66bacfd430b11197d49678d3c44c6b3cf272
Tobias van de Ven <tobiasvdven@hotmail.com>
2022-03-19 21:19:48 +0100

    Testing basic form of static helper for simple fubs without setup

---
+++ src/Fub.Tests/FubTests.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+
+namespace Fub.Tests
+{
+	public class FubTests
+	{
+		public class BasicModel
+		{
+			public BasicModel(int integer, string text)
+			{
+				Integer = integer;
+				Text = text;
+			}
+
+			public int Integer { get; }
+			public string Text { get; }
+		}
+
+		[Fact]
+		public void Fub_WithNoOverrides_ReturnsDefault()
+		{
+			BasicModel fub = Fub<BasicModel>.Simple();
+
+			Assert.Equal(0, fub.Integer);
+			Assert.Equal(string.Empty, fub.Text);
+		}
+
+		[Fact]
+		public void Fub_WithStringOverride_ReturnsFub()
+		{
+			BasicModel fub = Fub<BasicModel>.Simple(b => b.Text, "Override");
+
+			Assert.Equal(0, fub.Integer);
+			Assert.Equal("Override", fub.Text);
+		}
+
+		[Fact]
+		public void Fub_WithTwoOverrides_ReturnsFub()
+		{
+			BasicModel fub = Fub<BasicModel>.Simple(b => b.Text, "Apple", b => b.Integer, 16);
+
+			Assert.Equal(16, fub.Integer);
+			Assert.Equal("Apple", fub.Text);
+		}
+	}
+}
---
+++ src/Fub/Fub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Fub
+{
+	public static class Fub<T> where T : notnull
+	{
+		private static Fubber<T> fubber = new FubberBuilder<T>().Build();
+
+		public static T Simple()
+		{
+			return fubber.Fub();
+		}
+
+		public static T Simple<TProperty>(Expression<Func<T, TProperty>> expression, TProperty value)
+		{
+			return fubber.Fub(expression, value);
+		}
+
+		public static T Simple<TProperty1, TProperty2>(
+			Expression<Func<T, TProperty1>> expression1, TProperty1 value1,
+			Expression<Func<T, TProperty2>> expression2, TProperty2 value2)
+		{
+			return fubber.Fub(expression1, value1, expression2, value2);
+		}
+	}
+}
