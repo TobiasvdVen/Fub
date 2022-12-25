989812d57fd0888a3f173fc8f6e1524325d72163
Tobias van de Ven <tobiasvdven@hotmail.com>
2022-12-25 17:22:14 +0100

    Improved error reporting of static Fub

--- src/Fub/Fub.cs
+++ src/Fub/Fub.cs
@@ -5,23 +5,24 @@ namespace Fub
 {
 	public static class Fub<T> where T : notnull
 	{
-		private static Fubber<T> fubber = new FubberBuilder<T>().Build();
+		private static Lazy<Fubber<T>> fubber = new Lazy<Fubber<T>>(() => new FubberBuilder<T>().Build());
+		private static Fubber<T> Fubber => fubber.Value;
 
 		public static T Simple()
 		{
-			return fubber.Fub();
+			return Fubber.Fub();
 		}
 
 		public static T Simple<TMember>(Expression<Func<T, TMember>> expression, TMember value)
 		{
-			return fubber.Fub(expression, value);
+			return Fubber.Fub(expression, value);
 		}
 
 		public static T Simple<TMember1, TMember2>(
 			Expression<Func<T, TMember1>> expression1, TMember1 value1,
 			Expression<Func<T, TMember2>> expression2, TMember2 value2)
 		{
-			return fubber.Fub(expression1, value1, expression2, value2);
+			return Fubber.Fub(expression1, value1, expression2, value2);
 		}
 	}
 }
